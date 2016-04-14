c9d65e28238fc15b216a75af1af5a2824ac0de9b
Paweł Trysła <pawel.trysla@gmail.com>
2016-04-14 12:29:07 +0200

    Fixed path issues.

---
+++ source/cs/solab3/PhysicalMemory.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solab3
+{
+    class PhysicalMemory
+    {
+        private List<int> m_Frames;
+        private int m_Size;
+
+        public PhysicalMemory(int frames)
+        {
+            
+        }
+
+        public int Size
+        {
+            get { return m_Size; }
+        }
+    }
+}
---
+++ source/cs/solab3/ReplaceAlgorithm.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solab3
+{
+    abstract class ReplaceAlgorimthm
+    {
+        protected PhysicalMemory m_Frames;
+        protected int m_PagesErrors;
+        protected List<int> m_Requests;
+
+        public ReplaceAlgorimthm(List<int> reqs, int frames)
+        {
+            m_Requests = reqs;
+            m_PagesErrors = 0;
+            m_Frames = new PhysicalMemory(frames);
+        }
+
+        public int PagesErrors
+        {
+            get
+            {
+                return m_PagesErrors;
+            }
+        }
+
+        public abstract void HandleRequest(int value);
+    }
+}
