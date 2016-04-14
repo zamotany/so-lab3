535eaa6731ee64b3505f3106413fca1b0636842a
Kostu96 <kostu96@gmail.com>
2016-04-14 13:37:05 +0200

    LRUAlgorithm

--- source/cs/solab3/LRUAlgorithm.cs
+++ source/cs/solab3/LRUAlgorithm.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace solab3
 {
-    class FIFOAlgorithm : ReplaceAlgorimthm
+    class LRUAlgorithm : ReplaceAlgorimthm
     {
-        public FIFOAlgorithm(List<int> reqs, int frames) : base(reqs, frames)
+        public LRUAlgorithm(List<int> reqs, int frames) : base(reqs, frames)
         {
 
         }
@@ -17,7 +17,14 @@ namespace solab3
         {
             for (int i = 0; i < m_Frames.Size; ++i)
                 if (m_Frames[i].Value == value)
+                {
+                    Frame temp = m_Frames[i];
+                    for (int j = i; j < m_Frames.Size - 1; ++j)
+                        m_Frames[j] = m_Frames[j + 1];
+
+                    m_Frames[m_Frames.Size - 1] = temp;
                     return;
+                }
 
             for (int i = 0; i < m_Frames.Size - 1; ++i)
                 m_Frames[i] = m_Frames[i + 1];
