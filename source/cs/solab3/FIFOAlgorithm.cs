801d9e8778fec90fe37744664ef773a00e3cdc34
Kostu96 <kostu96@gmail.com>
2016-04-14 12:41:43 +0200

    FIFO init

---
+++ source/cs/solab3/FIFOAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solab3
+{
+    class FIFOAlgorithm : ReplaceAlgorimthm
+    {
+        public FIFOAlgorithm(List<int> reqs, int frames) : base(reqs, frames)
+        {
+
+        }
+
+        public override void HandleRequest(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
