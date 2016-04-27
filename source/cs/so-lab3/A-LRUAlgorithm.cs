3a0e44d7b7d8cc5d3d59152b294b464331999ef5
zamotany <pawel.trysla@gmail.com>
2016-04-27 16:38:46 +0200

    Reimplemented 2nd chance algo. Still not so efficient tho.

--- source/cs/so-lab3/A-LRUAlgorithm.cs
+++ source/cs/so-lab3/A-LRUAlgorithm.cs
@@ -32,21 +32,28 @@ namespace solab3
                 if(!m_Frames[k].ReferenceBit)
                 {
                     m_Frames[k] = new Frame(value, true);
+                    moveToEnd(k);
                     found = true;
-                    for(int j = 0; j < m_Frames.Size; j++)
-                    {
-                        if (j != k)
-                            m_Frames[j].ReferenceBit = false;
-                    }
                 }
                 else
                 {
-
+                    m_Frames[k].ReferenceBit = false;
+                    moveToEnd(k);
+                    k = k > 0 ? k - 1 : 0;
                 }
                 k = k < m_Frames.Size ? k + 1 : 0;
             }
 
             ++m_PagesErrors;
+        }
+
+        private void moveToEnd(int frame)
+        {
+            Frame temp = m_Frames[frame];
+
+            for (int i = frame; i < m_Frames.Size - 1; i++)
+                m_Frames[i] = m_Frames[i + 1];
+            m_Frames[m_Frames.Size - 1] = temp;
         }
 
         public override string ToString()
